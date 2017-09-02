99b7c9fb0835578aab359c78f00a5305c927c1eb
Tony <Tony@gmail.com>
2017-09-02 20:35:44 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/goodweather/android/db/Province.java
@@ -0,0 +1,37 @@
+package com.goodweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 小言 on 2017/9/2.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
